2362501a56a20d24470f47610d69034b62af4346
samuel.santana <samuel.santana@rexturadvance.com.br>
2019-01-09 15:19:38 -0200

    Added library lombok for less o amount get and set from project

--- portfolio-java-core/src/main/java/org/portfolio/java/core/comum/ExcecaoDeValidacao.java
+++ portfolio-java-core/src/main/java/org/portfolio/java/core/comum/ExcecaoDeValidacao.java
@@ -1,5 +1,6 @@
 package org.portfolio.java.core.comum;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 
@@ -7,6 +8,7 @@ import org.springframework.validation.BindingResult;
  * Classe para formatar erros de validacao de um objeto em formato JSON
  *
  */
+@Getter
 public final class ExcecaoDeValidacao extends ExcecaoGenerica {
 
     private static final long serialVersionUID = 1L;
@@ -15,9 +17,5 @@ public final class ExcecaoDeValidacao extends ExcecaoGenerica {
     public ExcecaoDeValidacao(BindingResult bindingResult, String mensagem) {
         super(HttpStatus.BAD_REQUEST, mensagem);
         this.bindingResult = bindingResult;
-    }
-
-    public BindingResult getBindingResult() {
-        return this.bindingResult;
     }
 }
